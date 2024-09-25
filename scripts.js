3b22cb3e2164b236b053912da830f414fb648e6a
DavidAdam1323 <davidadamgtr@gmail.com>
2024-09-25 11:45:14 +0100

    Add input elements and variables for tip calculator logic.

--- scripts.js
+++ scripts.js
@@ -1,5 +1,13 @@
+// Quick Tip Calculator Logic
 
+//  Get the bill amount input ✅
+//  Get the tip percentage input ✅
+//  Get the number of people input ✅
+//  Get the element to display the total bill ✅
+//  Get the element to display the total per person ✅
 
-
-
-
+const billAmoutInput = document.querySelector("#bill-amount");
+const tipPercentageInput = document.querySelector("#tip-percentage");
+const numPeopleInput = document.querySelector("#num-people");
+const totalBillAmount = document.querySelector("#total-bill");
+const sharedBillPerPerson = document.querySelector("#shared-bill");
