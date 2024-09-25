dccc784b777c3330fc08a6080c0825b7e750ba11
DavidAdam1323 <davidadamgtr@gmail.com>
2024-09-25 12:07:52 +0100

    Implement tip calculation function with input conversions.

--- scripts.js
+++ scripts.js
@@ -6,8 +6,19 @@
 //  Get the element to display the total bill ✅
 //  Get the element to display the total per person ✅
 
-const billAmoutInput = document.querySelector("#bill-amount");
+const billAmountInput = document.querySelector("#bill-amount");
 const tipPercentageInput = document.querySelector("#tip-percentage");
 const numPeopleInput = document.querySelector("#num-people");
 const totalBillAmount = document.querySelector("#total-bill");
 const sharedBillPerPerson = document.querySelector("#shared-bill");
+
+// Function to calculate the bill and shared amount 
+//  Convert the bill amount input to a floating-point number ✅
+//  Convert the tip percentage input to a floating-point number ✅
+//  Convert the number of people input to an integer: 
+//    - Specify radix ensure the string is always interpreted as intended. ✅
+function calculateTip () {
+  const billAmount = parseFloat(billAmountInput.value);
+  const tipPercentage = parseFloat(tipPercentageInput.value);
+  const  numPeople = parseInt(numPeopleInput.value, 10);
+}
